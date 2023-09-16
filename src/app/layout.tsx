8dcc1c4517c70e8c9373d5884b0e4b4385409d68
SMC242 <56912943+SMC242@users.noreply.github.com>
2023-09-16 04:21:57 +0100

    WIP: header bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,27 @@
 import type { Metadata } from "next";
+import Image from "next/image";
 import "./globals.css";
 
 export const metadata = {
-  title: 'The Gungle Zone',
-}
+  title: "The Gungle Zone",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="bg-purple">
+      <body className=" text-red-600">
+        <header className="absolute top-0 left-0 flex w-full border border-gray-200">
+          <span className="text-yellow-400">Hello</span>
+          <a href="/">
+            {/* <Image src="/logo.png" alt="Our logo" width={50} height={50} /> */}
+          </a>
+        </header>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
