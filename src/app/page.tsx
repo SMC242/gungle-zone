e2a8289424aacff3c3999d1b80cf9bca83c1a6e5
SMC242 <56912943+SMC242@users.noreply.github.com>
2023-09-16 04:21:45 +0100

    Added checkboxes to recipe card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,41 @@
 import { Suspense } from "react";
 import Loading from "./loading";
-import {Ingredient} from "./api/recipes/route";
+import { Ingredient } from "./api/recipes/route";
 
 async function RecipeCard() {
   const res = await fetch("http://localhost:3000/api/recipes/", {
-    next: {revalidate: 60}
+    next: { revalidate: 60 },
   });
-  if (!res.ok)  return <p>I forgor 💀</p>
-  const recipe: {ingredients: Ingredient[]} = await res.json();
+  if (!res.ok) return <p>I forgor 💀</p>;
+  const recipe: { ingredients: Ingredient[] } = await res.json();
   return (
-    <a className="flex items-center justify-center h-screen">
-      <div id="recipe-card" className="p-6 bg-white border border-gray-200 rounded-lg shadow howver:bg-gray-100">
-            <ul className="m-6">
-              {recipe["ingredients"].map(ingredient => <li key={ingredient.name}>{ingredient.quantity} of {ingredient.name}</li>)}
-            </ul>
+    <div className="flex items-center justify-center h-screen">
+      <div
+        id="recipe-card"
+        className="p-6 bg-white border border-gray-200 rounded-lg shadow howver:bg-gray-100"
+      >
+        <ul className="m-6">
+          {recipe["ingredients"].map((ingredient) => (
+            <li key={ingredient.name}>
+              <label className="block">
+                <input type="checkbox" />
+                <span className="m-5">
+                  {ingredient.quantity} of {ingredient.name}
+                </span>
+              </label>
+            </li>
+          ))}
+        </ul>
       </div>
-    </a>
-  )
+    </div>
+  );
 }
 
 export default async function HomePage() {
   return (
-  <Suspense fallback={<Loading />}>
-    <RecipeCard />
-  </Suspense>
-  )
-}1
+    <Suspense fallback={<Loading />}>
+      <RecipeCard />
+    </Suspense>
+  );
+}
+1;
