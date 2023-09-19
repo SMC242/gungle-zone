833fa2e9696e6c7e8e22a2956e6e647ab34bf172
eilidh <56912943+SMC242@users.noreply.github.com>
2023-09-19 01:04:10 +0100

    Added favicon + logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,11 +13,40 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="bg-black text-white">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body className="bg-gradient-to-br from-purple-900 to-yellow">
         <header className="absolute top-0 left-0 flex w-full border p-3 border-slate-400 bg-green">
-          <a href="/">
-            <Image src="/logo.png" alt="Our logo" width={50} height={50} />
-          </a>
+          <div className="bg-black">
+            <a href="/">
+              <Image
+                src="/gungle-zone-tree.png"
+                alt="Our logo"
+                width={50}
+                height={50}
+              />
+            </a>
+          </div>
         </header>
         {children}
       </body>
